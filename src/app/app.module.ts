02b80e148cfe18813e6afc2194ae6574938d3ad8
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2021-06-14 21:40:40 +0530

    simple form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { GetInputValueComponent } from './get-input-value/get-input-value.compon
 import { PropertyBindComponent } from './property-bind/property-bind.component';
 import { ConditionalStatementComponent } from './conditional-statement/conditional-statement.component';
 import { SwitchCaseComponent } from './switch-case/switch-case.component';
-import { ForLoopComponent } from './for-loop/for-loop.component'
+import { ForLoopComponent } from './for-loop/for-loop.component';
+import { SimpleFormComponent } from './simple-form/simple-form.component'
 
 @NgModule({
   declarations: [
@@ -24,12 +26,14 @@ import { ForLoopComponent } from './for-loop/for-loop.component'
     PropertyBindComponent,
     ConditionalStatementComponent,
     SwitchCaseComponent,
-    ForLoopComponent
+    ForLoopComponent,
+    SimpleFormComponent
   ], //all the components contain in this module
   imports: [
     BrowserModule,
     AppRoutingModule,
     ModuleModule, //added the created module to access things inside it
+    FormsModule, //to implement forms we need to import this
   ],
   providers: [],
   bootstrap: [AppComponent] //which component want to show from this module
